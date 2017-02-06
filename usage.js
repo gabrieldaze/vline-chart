e4e8251c9a817aafe3e0837f88a0933651aafa23
Gabriel Azevedo <gabriel.azevedo@artedigitalinternet.com.br>
2017-02-06 15:59:05 -0200

    Adding primary functions

--- usage.js
+++ usage.js
@@ -1,1 +1,10 @@
-vline = new VLine()
+window.onload = function() {
+
+	vline = new VLine()
+	vline.setupFont('Courier', 16)
+	vline.setupVLineHeader('Titulo', ['N1','N2','N3','N4'])
+	vline.addElement('Elemento', 4)
+	vline.addElement('Teste', 2)
+	vline.addElement('Outro teste', 1)
+	vline.buildCanvas()
+}
--- vline.js
+++ vline.js
@@ -6,7 +6,63 @@ class Point {
 }
 
 class VLine {
+
+	// Class constructor
 	constructor() {
-		// Class constructor
+		// Create the element canvas
+		var canvas = document.createElement('canvas')
+		canvas.id = 'canvas'
+		this.canvas = canvas
+
+		// Get the canvas context
+		var context = canvas.getContext('2d')
+		this.context = context
+
+		// Initializing main variables
+		this.vLineElements = null
+		this.elementRow = []
+		this.fontFamily = null
+		this.fontSize = null
 	}
+
+	// Set up the chart font
+	setupFont(fontFamily, fontSize) {
+		this.fontFamily = fontFamily
+		this.fontSize = fontSize
+		this.context.font = fontSize + 'px ' + fontFamily
+	}
+
+	// Set up the vLine Elements
+	setupVLineHeader(title, arrayElements) {
+		this.vLineElements = {
+			'title':title,
+			'elements':arrayElements
+		}
+	}
+
+	// Add element row to the chart
+	addElement(title, elementMarker) {
+		if(title == '' || elementMarker == 0)
+			return alert('[ERROR] addElement: Missing parameter')
+		if(elementMarker > this.vLineElements.elements.length)
+			return alert('[ERROR] addElement: elementMarker out of bounds')
+		this.elementRow.push({
+			'title':title,
+			'element':elementMarker
+		});
+	}
+
+	// Set up the canvas settings
+	buildCanvas() {
+		var arrayStrings = []
+		var context = this.context
+		arrayStrings.push(parseInt(context.measureText(this.vLineElements.title).width))
+		this.vLineElements.elements.forEach(function(string) {
+			arrayStrings.push(parseInt(context.measureText(string).width))
+		});
+		this.elementRow.forEach(function(string) {
+			arrayStrings.push(parseInt(context.measureText(string.title).width))
+		});
+		var maxWidth = Math.max(...arrayStrings)
+	}  
 }
