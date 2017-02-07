ce9abee1191b4e07d3a47dd4dec6c06822acdc22
Gabriel Azevedo <gabriel.azevedo@artedigitalinternet.com.br>
2017-02-07 15:21:23 -0200

    Working on...

--- usage.js
+++ usage.js
@@ -1,10 +1,12 @@
 window.onload = function() {
 
 	vline = new VLine()
-	vline.setupFont('Courier', 16)
+	vline.setupFont('Courier', 16, 20)
 	vline.setupVLineHeader('Titulo', ['N1','N2','N3','N4'])
 	vline.addElement('Elemento', 4)
-	vline.addElement('Teste', 2)
-	vline.addElement('Outro teste', 1)
+	// vline.addElement('Teste', 2)
+	// vline.addElement('Outro teste', 1)
+	// vline.addElement('Mais um teste', 1)
+	// vline.addElement('Leveza', 1)
 	vline.buildCanvas()
 }
--- vline.js
+++ vline.js
@@ -26,10 +26,11 @@ class VLine {
 	}
 
 	// Set up the chart font
-	setupFont(fontFamily, fontSize) {
+	setupFont(fontFamily, fontSize, fontMargin) {
 		this.fontFamily = fontFamily
 		this.fontSize = fontSize
-		this.context.font = fontSize + 'px ' + fontFamily
+		this.fontMargin = fontMargin
+		// this.context.font = fontSize + 'px ' + fontFamily
 	}
 
 	// Set up the vLine Elements
@@ -56,13 +57,46 @@ class VLine {
 	buildCanvas() {
 		var arrayStrings = []
 		var context = this.context
-		arrayStrings.push(parseInt(context.measureText(this.vLineElements.title).width))
+
+		var goldenRatio = 1.618
+		var goldenLine = this.fontSize * goldenRatio
+		var widthBase = (goldenLine * goldenLine) / this.fontSize
+		var charWidth = widthBase // / Math.sqrt(this.fontSize)
+
+		var titleArray = []
+		var maxTitleLength = 0
+		var elementArray = []
+		var maxElementLength = 0
+		titleArray.push(this.vLineElements.title.length)
 		this.vLineElements.elements.forEach(function(string) {
-			arrayStrings.push(parseInt(context.measureText(string).width))
+			elementArray.push(parseInt(string.length))
 		});
 		this.elementRow.forEach(function(string) {
-			arrayStrings.push(parseInt(context.measureText(string.title).width))
+			titleArray.push(string.title.length)
 		});
-		var maxWidth = Math.max(...arrayStrings)
-	}  
+		maxTitleLength = Math.max(...titleArray)
+		maxElementLength = Math.max(...elementArray)
+		this.canvas.width = (charWidth * maxTitleLength) + (charWidth * maxElementLength) + (this.fontMargin * this.elementRow.length) + 2 * this.fontMargin
+		this.canvas.height = (this.fontSize + this.fontMargin * 2) + (this.fontSize + this.fontMargin * 2) * this.elementRow.length
+		document.body.appendChild(this.canvas)
+		this.drawTable()
+	} 
+
+	// Draw the table
+	drawTable() {
+		this.context.font = this.fontSize + 'px ' + this.fontFamily
+		this.context.fillStyle = '#EEE'
+		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+		this.context.fillStyle = '#333'
+		this.context.fillText(this.vLineElements.title, this.fontMargin, this.fontMargin * 1.4)
+		var currentX = this.fontMargin
+		var currentY = this.fontMargin * 1.4 + this.fontSize * 4
+		var context = this.context
+		var fontSize = this.fontSize
+		var fontMargin = this.fontMargin
+		this.elementRow.forEach(function(obj) {
+			context.fillText(obj.title, currentX, currentY)
+			currentY += fontSize + fontMargin * 1.4
+		});
+	}
 }
